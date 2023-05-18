0557d05a0b22490dbae41d7eeaaeb0a1f7dcdd65
root <root@ip-172-31-40-41.eu-central-1.compute.internal>
2023-05-18 12:02:43 +0000

    add

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <div class="container px-lg-5">
             <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
                 <div class="m-4 m-lg-5">
-                    <h1 class="display-5 fw-bold">iConnect cicd pipeline</h1>
+                    <h1 class="display-5 fw-bold">iConnect cccccccicd pipeline</h1>
                     <p class="fs-4"><ol>
                 <?php
                 foreach ($_["users"] as $user) {
